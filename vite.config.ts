ca81a019c937f871133075777f04e6a481675eb5
Sebastian Nosal <nosal.sebastian16@gmail.com>
2024-10-24 21:52:50 +0200

    Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'Rumblefish-Recruitment',
+  base: '/Rumblefish-Recruitment/',
   css: {
     postcss: {
       plugins: [tailwindcss()]
